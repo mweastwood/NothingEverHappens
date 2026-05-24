0d9fc034b58b7e005d8554b9c8305aad95cc14c8
Michael Eastwood <michael.w.eastwood@gmail.com>
2026-05-23 17:55:01 -0700

    feat: enable offline Firestore persistence on Web and register ErrorHandler

--- app/lib/main_prod.dart
+++ app/lib/main_prod.dart
@@ -1,4 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options_prod.dart';
 import 'main.dart';
@@ -6,5 +8,13 @@ import 'main.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Enable persistence for Web
+  if (kIsWeb) {
+    FirebaseFirestore.instance.settings = const Settings(
+      persistenceEnabled: true,
+    );
+  }
+
   mainCommon();
 }
