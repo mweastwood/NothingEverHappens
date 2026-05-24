dd495b06a559fe041c51008165c426ca5c581311
Michael Eastwood <michael.w.eastwood@gmail.com>
2026-05-23 20:06:30 -0700

    fix: ignore deprecated axisAlignment warning in SizeTransition

--- app/lib/widgets/task_widget.dart
+++ app/lib/widgets/task_widget.dart
@@ -115,6 +115,7 @@ class _TaskWidgetState extends State<TaskWidget>
         return SizeTransition(
           sizeFactor: _sizeFactorAnimation,
           axis: Axis.vertical,
+          // ignore: deprecated_member_use
           axisAlignment: -1.0,
           child: transformedChild,
         );
