207b4fc5d741959dc90388849a5679d027d3d93e
Michael Eastwood <michael.w.eastwood@gmail.com>
2026-06-04 18:22:48 -0500

    test: remove PR suffix from test name (PR 5/5)

--- app/test/screens/help_screen_test.dart
+++ app/test/screens/help_screen_test.dart
@@ -233,9 +233,7 @@ void main() {
     expect(find.byType(FunCheckButton), findsNWidgets(3));
   });
 
-  testGoldens('HelpScreen renders correctly (PR 5/5)', (
-    WidgetTester tester,
-  ) async {
+  testGoldens('HelpScreen renders correctly', (WidgetTester tester) async {
     await tester.pumpWidgetBuilder(
       const HelpScreen(),
       wrapper: l10nMaterialAppWrapper(),
