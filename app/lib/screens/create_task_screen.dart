7f382e3c9a53a3d4b9e206b70ee9b0c0f69824f4
Michael Eastwood <michael.w.eastwood@gmail.com>
2026-08-05 23:16:05 -0700

    fix: remove unused import and format code

--- app/lib/screens/create_task_screen.dart
+++ app/lib/screens/create_task_screen.dart
@@ -10,7 +10,6 @@ import '../logic/error_handler.dart';
 import '../logic/l10n_extension.dart';
 import 'package:cloud_firestore/cloud_firestore.dart' hide Type;
 import '../logic/family_repository.dart';
-import '../logic/family.dart';
 import '../logic/undo_notifier.dart';
 import '../widgets/undo_snackbar.dart';
 
