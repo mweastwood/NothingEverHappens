6d0b21525fe55eb09cdc94cde1a05901c89651bf
Michael Eastwood <michael.w.eastwood@gmail.com>
2026-08-14 19:49:54 +0000

    fix(create_task_screen): remove redundant family_role import

--- app/lib/screens/create_task_screen.dart
+++ app/lib/screens/create_task_screen.dart
@@ -1,4 +1,3 @@
-import 'package:nothing_ever_happens/logic/family_role.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
