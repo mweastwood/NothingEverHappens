eec3c9d1d86b3058f56440615f0bd86c1675eae4
Michael Eastwood <michael.w.eastwood@gmail.com>
2026-08-14 10:32:28 +0000

    fix(logic): remove unnecessary task_status import in hive_local_data_source

--- app/lib/logic/hive_local_data_source.dart
+++ app/lib/logic/hive_local_data_source.dart
@@ -6,7 +6,6 @@ import 'package:nothing_ever_happens/logic/civil_day.dart';
 import 'package:nothing_ever_happens/logic/relative_time.dart';
 import 'package:nothing_ever_happens/logic/task_instance.dart';
 import 'package:nothing_ever_happens/logic/task_schedule.dart';
-import 'package:nothing_ever_happens/logic/task_status.dart';
 import 'package:nothing_ever_happens/logic/user_settings.dart';
 import 'package:rxdart/rxdart.dart';
 
