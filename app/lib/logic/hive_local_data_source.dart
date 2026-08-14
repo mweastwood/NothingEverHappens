bc69ffa38014ca16d7655fff46bbcdf840fc9c6a
Michael Eastwood <michael.w.eastwood@gmail.com>
2026-08-14 10:30:16 +0000

    fix(logic): restore required cloud_firestore and flutter/material imports in hive_local_data_source

--- app/lib/logic/hive_local_data_source.dart
+++ app/lib/logic/hive_local_data_source.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:nothing_ever_happens/logic/civil_day.dart';
