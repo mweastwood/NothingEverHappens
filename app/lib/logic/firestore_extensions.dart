9aa9c2c41b2afd4dc16e3b8513c62e02200a5796
Michael Eastwood <michael.w.eastwood@gmail.com>
2026-08-15 23:14:03 +0000

    test(firestore): add safeGet unit tests and support GetOptions parameter

--- app/lib/logic/firestore_extensions.dart
+++ app/lib/logic/firestore_extensions.dart
@@ -4,22 +4,24 @@ import 'package:flutter/foundation.dart';
 
 extension FirestoreDocFetchExtension<T> on DocumentReference<T> {
   Future<DocumentSnapshot<T>> safeGet({
+    GetOptions? options,
     Duration timeout = const Duration(seconds: 10),
   }) async {
     if (kIsWeb) {
       return snapshots().first.timeout(timeout);
     }
-    return get().timeout(timeout);
+    return get(options).timeout(timeout);
   }
 }
 
 extension FirestoreQueryFetchExtension<T> on Query<T> {
   Future<QuerySnapshot<T>> safeGet({
+    GetOptions? options,
     Duration timeout = const Duration(seconds: 10),
   }) async {
     if (kIsWeb) {
       return snapshots().first.timeout(timeout);
     }
-    return get().timeout(timeout);
+    return get(options).timeout(timeout);
   }
 }
