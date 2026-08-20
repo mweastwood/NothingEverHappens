2ae9fd130a92b7b486defe4f4df4e036afce77e2
Michael Eastwood <michael.w.eastwood@gmail.com>
2026-08-20 10:13:56 +0000

    style: format family_repository and refresh golden

--- app/lib/logic/family_repository.dart
+++ app/lib/logic/family_repository.dart
@@ -289,11 +289,9 @@ class FamilyRepository {
             batch.update(familyRef, {
               'members.${nextParent.userId}.role': FamilyRole.parent.toJson(),
             });
-            batch.set(
-              _firestore.collection('users').doc(nextParent.userId),
-              {'familyRole': FamilyRole.parent.toJson()},
-              SetOptions(merge: true),
-            );
+            batch.set(_firestore.collection('users').doc(nextParent.userId), {
+              'familyRole': FamilyRole.parent.toJson(),
+            }, SetOptions(merge: true));
           }
         }
       }
